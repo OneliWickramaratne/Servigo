a548ac6e7b36ffa520086299c185c9527d19095a
Oneli <onithesanya@gmail.com>
2025-07-17 10:27:39 +0530

    package,index,user updated

---
+++ backend/models/user.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+//structure of user data in database
+//** mongoose is a library that helps to connect with database and create schema */
+
+//create schema for user
+const userSchema = new mongoose.Schema({
+  
+    email :{
+        type : String,
+        required : true,
+        unique : true
+    },
+    password :
+    {
+        type : String,
+        required :true
+    },
+    isBlocked :{
+        type : Boolean,
+        required : true,
+        default: false
+    },
+    role :
+    {
+        type : String,
+        required : true,
+        default : "customer"
+        //** if you not mention user type,
+        // it all ready store type is customer */
+    },
+    firstname :
+    {
+        type : String,
+        required : true
+    },
+    lastname :
+    {
+        type : String,
+        required : true
+    },
+    address :
+    {
+        type : String,
+        required : true
+    },
+    phoneNumber :
+    {
+        type : String,
+        required:true
+    },
+    profilePicture :
+    {
+       type : String,
+       required : true, 
+       default :"https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fuser%2F&psig=AOvVaw0uCU9t72Sm51_RBTIJ2OxU&ust=1739456529253000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMCbiuCqvosDFQAAAAAdAAAAABAE"
+    }
+
+});
+
+//create a database collection for saving user values,
+//** create  to relationship between backend and database collection */
+const User = mongoose.model("User",userSchema);
+
+
+export default User;
